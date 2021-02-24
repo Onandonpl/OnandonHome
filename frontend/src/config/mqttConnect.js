97ccbc801afe7dea6ccab548b35f87364f3aab5e
Robert Mehring <onandonpl@gmail.com>
2021-02-24 14:08:47 +0100

    disable mqtt for firebase hosting

--- frontend/src/config/mqttConnect.js
+++ frontend/src/config/mqttConnect.js
@@ -1,9 +1,9 @@
-import mqtt from "mqtt";
-import dotenv from "dotenv";
-dotenv.config();
-const options = {
-  clientId: "REACT",
-};
-const client = mqtt.connect(process.env.REACT_APP_MQTT_WS, options);
-client.setMaxListeners(100);
-export default client;
+// import mqtt from "mqtt";
+// import dotenv from "dotenv";
+// dotenv.config();
+// const options = {
+//   clientId: "REACT",
+// };
+// const client = mqtt.connect(process.env.REACT_APP_MQTT_WS, options);
+// client.setMaxListeners(100);
+// export default client;
