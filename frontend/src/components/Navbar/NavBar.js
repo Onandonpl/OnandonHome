0812f65843e33792257d61550fed7f0072987af9
Robert Mehring <onandonpl@gmail.com>
2021-02-24 16:08:40 +0100

    update navbar

--- frontend/src/components/Navbar/NavBar.js
+++ frontend/src/components/Navbar/NavBar.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import styled from "styled-components/macro";
 import Burger from "./Burger";
 import Menu from "./Menu";
-import CC2531 from "../Devices/Device/CC2531/CC2531";
 const NavBar = () => {
   const [open, setOpen] = useState(false);
   const handleNavBar = () => setOpen((state) => !state);
@@ -10,7 +9,6 @@ const NavBar = () => {
   return (
     <NavBarContainer>
       <Burger open={open} setOpen={handleNavBar} />
-      <CC2531 />
       <Menu open={open} setOpen={handleNavBar} />
     </NavBarContainer>
   );
