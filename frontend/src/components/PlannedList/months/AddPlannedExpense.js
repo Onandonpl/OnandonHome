3b06915e797cb9fa341de1a3eaf966783016df46
Robert Mehring <onandonpl@gmail.com>
2021-02-24 15:49:26 +0100

    update add options

--- frontend/src/components/PlannedList/months/AddPlannedExpense.js
+++ frontend/src/components/PlannedList/months/AddPlannedExpense.js
@@ -4,13 +4,33 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import { useForm } from "react-hook-form";
 import { useAlert } from "react-alert";
+import { useCollection } from "react-firebase-hooks/firestore";
 
+import firebase from "../../../config/firebase";
 import { addPlannedExpense } from "../../../utils/firebase/firebaseFunctions";
 
 const AddPlannedExpense = ({ categoryId, monthId }) => {
+  const [categories, loading] = useCollection(
+    firebase.firestore().collection("categories").orderBy("created", "asc")
+  );
   const alert = useAlert();
   const [value, onChange] = useState(new Date());
   const { register, handleSubmit, reset } = useForm();
+
+  if (loading) return <> </>;
+
+  const filteredByCategory = categories.docs.map((value) => value);
+
+  const selectCategories = (type) => {
+    const filtered = filteredByCategory.filter((value) =>
+      value.data().categoryType === type ? value : null
+    );
+    return filtered.map((value) => (
+      <option key={value.id} value={value.data().categoryName}>
+        {value.data().categoryName}
+      </option>
+    ));
+  };
 
   const handleAddTransaction = (data) => {
     const timeWhenAdded = new Date().toLocaleString().split(",")[1];
@@ -22,7 +42,9 @@ const AddPlannedExpense = ({ categoryId, monthId }) => {
       data.expense,
       convertAmount,
       value,
-      timeWhenAdded
+      timeWhenAdded,
+      data.mainCategory,
+      data.subCategory
     );
 
     alert.show(`Dodano ${data.expense}`);
@@ -46,7 +68,13 @@ const AddPlannedExpense = ({ categoryId, monthId }) => {
           placeholder="Kwota"
           name="amount"
           ref={register({ required: true, min: 0 })}
-        />
+        />{" "}
+        <Select name="mainCategory" ref={register}>
+          {selectCategories("mainCategory")}
+        </Select>
+        <Select name="subCategory" ref={register}>
+          {selectCategories("subCategory")}
+        </Select>
         <ButtonSubmit type="submit" value="+" />
       </Form>
     </TransactionAddContainer>
