bf0f711d62fa6e61a599f09e38b25991b490588b
Robert Mehring <onandonpl@gmail.com>
2021-04-09 17:52:05 +0200

    add alert lib

---
+++ frontend/src/lib/alertOptions.js
@@ -0,0 +1,8 @@
+import { transitions, positions } from "react-alert";
+
+export const alertOptions = {
+  position: positions.TOP_RIGHT,
+  timeout: 3000,
+  offset: "30px",
+  transition: transitions.FADE,
+};
