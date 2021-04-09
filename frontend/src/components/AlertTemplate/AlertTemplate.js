6427a70aec754c6a80a849385a3d60570264a8bc
Robert Mehring <onandonpl@gmail.com>
2021-04-09 17:50:27 +0200

    add alert

---
+++ frontend/src/components/AlertTemplate/AlertTemplate.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { Alert } from "./style";
+const AlertTemplate = ({ message }) => <Alert>{message}</Alert>;
+export default AlertTemplate;
---
+++ frontend/src/components/AlertTemplate/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components/macro";
+
+export const Alert = styled.div`
+  min-width: 300px;
+  max-width: 400px;
+
+  padding: 15px;
+  margin: 20px;
+
+  overflow: hidden;
+
+  display: flex;
+
+  background-color: #131931;
+
+  color: #ffffff;
+
+  z-index: 500;
+`;
