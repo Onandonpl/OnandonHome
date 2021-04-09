96b0b9450b7018eb12f9192e6c158e9dffdf0aa8
Robert Mehring <onandonpl@gmail.com>
2021-04-09 17:50:12 +0200

    add providers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 // import reportWebVitals from "./reportWebVitals";
 
+import { Provider as AlertProvider } from "react-alert";
+import { alertOptions } from "./lib/alertOptions";
+import AlertTemplate from "./components/AlertTemplate/AlertTemplate";
+
+import Providers from "./context/Providers";
 ReactDOM.render(
-  <>
-    <App />
-  </>,
+  <AlertProvider template={AlertTemplate} {...alertOptions}>
+    <Providers>
+      <App />
+    </Providers>
+  </AlertProvider>,
   document.getElementById("root")
 );
 
